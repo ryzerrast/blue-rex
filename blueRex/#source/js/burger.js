575e418b74a3805ebdd7ca9ecc6d98869720c352
Dmitry Zakhoda <68736899+ryzerrast@users.noreply.github.com>
2021-02-15 22:50:51 +0200

    ok

--- blueRex/#source/js/burger.js
+++
@@ -1,7 +0,0 @@
-const iconMenu = document.querySelector('.icon-menu'),
-      menuBody = document.querySelector('.menu__body');
-
-iconMenu.addEventListener('click', function() {
-   iconMenu.classList.toggle('active');
-   menuBody.classList.toggle('active');
-});
